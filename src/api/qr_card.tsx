642039e4c44f8a517bcf3d57dc9f9acde9b69eab
Kaid <kimdiannebagasona@gmail.com>
2024-05-18 09:10:32 +0800

    update lib loc

--- src/api/qr_card.tsx
+++ src/api/qr_card.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase'; // Import Supabase client
+import { supabase } from '../../lib/supabase'; // Import Supabase client
 
 interface QrCard {
   id: number;
--- src/api/transaction.tsx
+++ src/api/transaction.tsx
@@ -1,6 +1,6 @@
 
 import { useState, useEffect } from 'react';
-import { supabase } from '@/lib/supabase'; // Import Supabase client
+import { supabase } from '../../lib/supabase'; // Import Supabase client
 
 export interface TransactionData {
   item_id: number;
